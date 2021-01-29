f084dfae84a108c17071ac2078e98d2b2d713981
Aissaoui Ahmed <20658625+Aissaoui-Ahmed@users.noreply.github.com>
2021-01-29 16:13:11 +0100

    Update main.js
    Remove typeahead.js
    Add datatables

--- js/main.js
+++ js/main.js
@@ -1,46 +1,9 @@
-var TermsEN = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('wEN'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: 'https://raw.githubusercontent.com/softvenue/i18n/master/i18n.json'
+$(document).ready(function () {
+  $("#table_id").DataTable({
+    ajax: {
+      url: "https://raw.githubusercontent.com/softvenue/i18n/master/i18n.json",
+      dataSrc: "",
+    },
+    columns: [ { data: "wEN" },{ data: "wAR" }],
   });
-  
-  var TermsAR = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('wAR'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: 'https://raw.githubusercontent.com/softvenue/i18n/master/i18n.json'
-  });
-  
-  TermsEN.clear();  
-  TermsEN.clearPrefetchCache();
-  TermsEN.clearRemoteCache();
-  TermsEN.initialize(true);
-  TermsAR.clear();
-  TermsAR.clearPrefetchCache();
-  TermsAR.clearRemoteCache();
-  TermsAR.initialize(true);
-  
-  $('#multiple-datasets .typeahead').typeahead({
-    highlight: true,
-  },
-  {
-    name: 'enTerms',
-    display: 'wEN',
-    source: TermsEN,
-    templates: {
-      header: '<h3 class="ar">مصطلحات Terms</h3>',
-      suggestion: function (data) {
-        return '<p><strong>' + data.wAR + '</strong> = ' + data.wEN + '</p>';
-      }
-    }
-  },
-  {
-    name: 'arTerms',
-    display: 'wAR',
-    source: TermsAR,
-    templates: {
-      header: '<h3 class="ar">Terms مصطلحات</h3>',
-      suggestion: function (data) {
-        return '<p><strong>' + data.wEN + '</strong> = ' + data.wAR + '</p>';
-      }
-    }
-  });
+});
