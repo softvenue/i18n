60e0f1199bcbce6e95bd4ca1b0f69d8d2e3efa7f
Aissaoui Ahmed <20658625+Aissaoui-Ahmed@users.noreply.github.com>
2021-01-31 21:17:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function () {
-  $("#table_id").DataTable({
-    ajax: {
-      url: "https://raw.githubusercontent.com/softvenue/i18n/master/i18n.json",
-      dataSrc: "",
-    },
-    columns: [ { data: "wEN" },{ data: "wAR" }],
-  });
-});
+$(document).ready(function() {
+  $('#table_id').DataTable();
+} );
